3adc5a4c55746333e57d59e68a1fc88d83dc8d97
William Barrett Simms <wbsimms@gmail.com>
2013-09-18 07:38:06 -0400

    Initial Revision

---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace Demo1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Hello World!");
+        }
+
+    }
+}
